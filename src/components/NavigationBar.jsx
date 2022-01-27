cc44fa9ffab9c695fc9d3d3e5158d32f9b28b5e6
valdez-erardtimothy <valdez.erardtimothy@gmail.com>
2022-01-27 22:24:57 +0800

    add dashboard link on navbar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -20,13 +20,7 @@ export default function NavigationBar() {
             <Nav className="me-auto">
                 <Nav.Link as={Link} to="/">Home</Nav.Link>
                 <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
+                
                 {!isAuthenticated ? (
                     <Nav.Link as={Link} to="/login">Log in</Nav.Link>) : (
                     <NavDropdown
@@ -34,12 +28,18 @@ export default function NavigationBar() {
                         id="nav-authenticated-dropdown"
                     >
                         <NavDropdown.Item 
+                            as={Link}
+                            to="/teller"
+                        >
+                            Dashboard
+                        </NavDropdown.Item>
+                    
+                        <NavDropdown.Item 
                             as="button"
                             onClick={()=>{dispatch(logout());}}
                         >
                             Log out
                         </NavDropdown.Item>
-                        
                     </NavDropdown>
                 )}
             </Nav>
