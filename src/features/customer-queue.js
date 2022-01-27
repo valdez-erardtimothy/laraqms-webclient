6c83ceece3e1cac77874155f0c93694c9de6eea5
valdez-erardtimothy <valdez.erardtimothy@gmail.com>
2022-01-28 02:06:27 +0800

    load queue data feature method

--- src/features/customer-queue.js
+++ src/features/customer-queue.js
@@ -5,15 +5,34 @@ export const enqueue = createAsyncThunk(
     'customer/queue/enqueue',
     async function(formData) {
         let response  = await axios.post('/api/enqueue', formData);
-
+        // sets cookie on success
         return response.data;
     }
 );
 
+/**
+ * loads queue number based on token cookie
+ */
+export const loadQueueNumber = createAsyncThunk(
+    'customer/queue/getQueue',
+    async function() {
+        let response = await axios.get('/api/get-queue-slot');
+
+        return response.data;
+    }
+); 
+
 const customerQueueSlice = createSlice({
     'name': "customer/queue",
-    initialState:{},
-    
+    initialState:{
+        queueData:null
+    },
+    extraReducers: builder => {
+        builder
+            .addCase(loadQueueNumber.fulfilled, (state, action) => {
+                state.queueData = action.payload.customer;
+            });
+    }
 }); 
 
 
