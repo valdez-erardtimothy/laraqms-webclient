d48f6378558de3773e46874df9a1508bf942c385
valdez-erardtimothy <valdez.erardtimothy@gmail.com>
2022-01-27 22:25:11 +0800

    add navbar on teller layout

--- src/teller/Layout.jsx
+++ src/teller/Layout.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Container} from 'react-bootstrap';
 import {Outlet} from 'react-router-dom';
+import NavigationBar from '../components/NavigationBar';
 export default function Layout(){
     return <Container fluid className="g-0">
+        <NavigationBar/>
         <Outlet/>
     </Container>;
 }
